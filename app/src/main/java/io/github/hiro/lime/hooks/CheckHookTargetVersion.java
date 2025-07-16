5cf4409d6edd7fa290b97ebba7a7288136a56679
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-07-16 10:47:08 +0900

    CheckHookTargetVersion.java の更新

--- app/src/main/java/io/github/hiro/lime/hooks/CheckHookTargetVersion.java
+++ app/src/main/java/io/github/hiro/lime/hooks/CheckHookTargetVersion.java
@@ -33,7 +33,7 @@ public class CheckHookTargetVersion implements IHook {
                                 !(isVersionInRange(versionName, "15.3.0", "15.4.0")) &&
                                 !(isVersionInRange(versionName, "15.4.0", "15.4.2"))&&
                                 !(isVersionInRange(versionName, "15.5.0", "15.8.0"))&&
-                                !(isVersionInRange(versionName, "15.9.0", "15.10.0"))
+                                !(isVersionInRange(versionName, "15.9.0", "15.12.0"))
                         ) {
                                  Utils.addModuleAssetPath(context);
                             Toast.makeText(context.getApplicationContext(), context.getString(R.string.incompatible_version), Toast.LENGTH_SHORT).show();
